eba21070782861b91d8b85b0c232a909aca6bf20
akira-cn <akira.cn@gmail.com>
2020-03-23 16:38:01 +0800

    add command-ui

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ import {options} from './lib/cmd.js';
 import {loadCorpus, saveCorpus} from './lib/corpus.js';
 import {generate} from './lib/generator.js';
 import {createRandomPicker} from './lib/random.js';
+import {answer} from './lib/answer.js';
 
 const corpus = loadCorpus('corpus/data.json');
-const title = options.title || createRandomPicker(corpus.title)();
-const article = generate(title, {corpus, ...options});
-saveCorpus(title, article);
+let title = options.title || createRandomPicker(corpus.title)();
+
+(async function () {
+  if(Object.keys(options).length <= 0) {
+    const answers = await answer([
+      {text: '请输入文章主题', value: title},
+      {text: '请输入最小字数', value: 6000},
+      {text: '请输入最大字数', value: 10000},
+    ]);
+    title = answers[0];
+    options.min = answers[1];
+    options.max = answers[2];
+  }
+
+  const article = generate(title, {corpus, ...options});
+  const output = saveCorpus(title, article);
+
+  console.log(`生成成功！文章保存于：${output}`);
+}());
