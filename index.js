663bfee8bb49937f47c78808a931eaca42c6e2fa
akira-cn <akira.cn@gmail.com>
2020-03-23 18:40:43 +0800

    升级交互命令

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import {options} from './lib/cmd.js';
 import {loadCorpus, saveCorpus} from './lib/corpus.js';
 import {generate} from './lib/generator.js';
 import {createRandomPicker} from './lib/random.js';
-import {answer} from './lib/answer.js';
+import {interact} from './lib/interact.js';
 
 const corpus = loadCorpus('corpus/data.json');
 let title = options.title || createRandomPicker(corpus.title)();
 
 (async function () {
   if(Object.keys(options).length <= 0) {
-    const answers = await answer([
+    const answers = await interact([
       {text: '请输入文章主题', value: title},
       {text: '请输入最小字数', value: 6000},
       {text: '请输入最大字数', value: 10000},
